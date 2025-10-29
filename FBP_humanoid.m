1f43ac041e15cd79824b5c740d2bda8c7f95fc10
zyz-FBP <zzz5527@psu.edu>
2025-10-28 21:42:47 -0400

    Update FBP_humanoid.m

--- FBP_humanoid.m
+++ FBP_humanoid.m
@@ -2,13 +2,15 @@ clear
 clc
 format long
 
-%% visualization setting
+%% visualization setting for boundary evolution
 is_viz = true;
 
+% Number of sampling points
 N = [400];
 
-%initial condition
+%% initial condition
 for rr = 1:length(N)
+%first generate 25 points
 pts=[...
 2.0, 7.8
 3.0, 7.5
@@ -35,10 +37,13 @@ pts=[...
 0.4, 6.8
 1.0, 7.5
 2.0, 7.8];
+%make the curve more smooth
 x_data = (pts(:,1)-2.0);
 y_data = 1.5*(pts(:,2));
 x_data(x_data<0) = x_data(x_data<0) - 0.2;
 x_data(x_data>0) = x_data(x_data>0) + 0.2;
+% Use cubic spline interpolation to obtain the initial curve which consists
+% of N points and make all of them equally spaced
 t_data = 1:length(x_data);
 t_query = linspace(t_data(1),t_data(end),N+1);
 xx = spline(t_data,x_data,t_query); 
@@ -54,13 +59,15 @@ new_curve(:,1) = interp1(deltas, scurve(:,1),target_arclength,'spline');
 new_curve(:,2) = interp1(deltas, scurve(:,2), target_arclength, 'spline');
 xxx = new_curve(1:N,1);
 yyy = new_curve(1:N,2);
-
+%shrink the curve
 xxx = xxx./4;
 yyy = yyy./4;
 
 curve0 = [xxx,yyy];
 curve = curve0;
 theta0 = [0:2*pi/N:2*pi-2*pi/N]';
+% Here tvec0 is used to align the vectors obtained from local
+% svd so that the tangent vectors are counterclockwise
 tvec0 = [-sin(theta0) cos(theta0)];
 nvec0 = [-cos(theta0) -sin(theta0)];
 [tvec,~,~] = localsvd(curve,21,1,tvec0,nvec0);
@@ -77,29 +84,45 @@ nvec(:,2) = -tvec(:,1);
 told = tvec;
 nold = nvec;
 
+%time step size
 dt = 1.0e-5;
+%start time
 time = 0.;
+%count the number of resampling procedure
 resample = 0;
-time_end = 15.0;
+%end time
+time_end = 2.1;
+%total time steps
 T = time_end/dt;
+%count the time step
 tdt = 0;
 
 while time <= time_end+dt
-    
     [A,b,t_gmls,n_gmls,rightpt,alpha,center_x,center_y] = velocity(curve,N,told,nold);
     told = t_gmls';
     nold = -n_gmls';
 
+    % velocity in normal direction
     d_curve = -A\b;
     vn = n_gmls'.*d_curve;
-    
-    max_radius = max(sqrt((curve(:,1)-center_x).^2 + (curve(:,2)-center_y).^2));
-    min_radius = min(sqrt((curve(:,1)-center_x).^2 + (curve(:,2)-center_y).^2));
+    %max distance and min distance from boundary points to centroid
+    max_radius(tdt+1,1) = max(sqrt((curve(:,1)-center_x).^2 + (curve(:,2)-center_y).^2));
+    min_radius(tdt+1,1) = min(sqrt((curve(:,1)-center_x).^2 + (curve(:,2)-center_y).^2));
     curve_old = curve;
+    %keep record of boundary at sepcific time to plot them later
+    if tdt == 1000
+    curve_t1 = curve;
+    elseif tdt == 6000
+        curve_t2 = curve;
+    elseif tdt == 2.1e05
+        curve_t3 = curve;
+    end
+
+    %% update boundary after dt
     %forward euler
     curve = curve + dt.*(n_gmls'.*d_curve);
    
-    %% Resampling if necessary
+    %% Resampling by cubic spline interpolation if necessary to prevent points from collapsing
     scurve = curve;
     new_curve = zeros(size(scurve));
     dx = diff(scurve(:,1));
@@ -111,30 +134,42 @@ while time <= time_end+dt
     new_curve(:,2) = interp1(deltas, scurve(:,2), target_arclength, 'spline');
     curve = new_curve;
     resample = resample + 1;
-
-    time = time + dt;
-    tdt = tdt + 1;
   
    if is_viz
-        subplot(2,2,1)
-        plot(curve_old(:,1), curve_old(:,2),'ro');
-        subplot(2,2,2)
-        quiver(curve_old((1:1:end),1),curve_old((1:1:end),2),n_gmls(1,1:1:end)',n_gmls(2,1:1:end)','r')
-        title('Outer normal vector')
-        subplot(2,2,3)
-        quiver(curve_old((1:1:end),1),curve_old((1:1:end),2),vn(1:1:end,1),vn(1:1:end,2),'r')
+        figure (1)%plot boundary evolution and its initial boundary
+        plot([curve0(:,1);curve0(1,1)], [curve0(:,2);curve0(1,2)],'c','LineWidth',2,'MarkerSize',5)
         hold on
-        plot(curve0((1:1:end),1),curve0((1:1:end),2),'g')
+        plot(curve_old(:,1), curve_old(:,2),'k--','LineWidth',2,'MarkerSize',5);
         hold off
-        title('Motion of curve')
-        subplot(2,2,4)
-        plot(d_curve)
-        title(['t=',num2str(tdt-1)])
-        hold off
+        xlim([-1 1])
+        ylim([0 3.0])
+        legend('Initial Boundary','Current Boundary','Location','northeast','interpreter','latex','FontSize',10)
+        title(['Boundary Moton at t=',num2str(tdt)])
         pause(0.1)
    end
+   time = time + dt;
+    tdt = tdt + 1;
     end
 end
+figure (2)%plot the boundary at specific times
+plot([curve0(:,1);curve0(1,1)], [curve0(:,2);curve0(1,2)],'k-','LineWidth',2,'MarkerSize',5);
+hold on
+plot(curve_t1(:,1), curve_t1(:,2),'b--','LineWidth',2,'MarkerSize',5);
+hold on
+plot(curve_t2(:,1), curve_t2(:,2),'m--','LineWidth',2,'MarkerSize',5);
+hold on
+plot(curve_t3(:,1), curve_t3(:,2),'r-.','LineWidth',2,'MarkerSize',5);
+hold off
+legend('$t=0$','$t=0.01$','$t=0.06$','$t=2.1$','Location','northeast','interpreter','latex','FontSize',14)
+figure (3)%plot the max distance and min distance every 100 time steps
+plot([0:100:2.1e05]'*1.0e-05,max_radius(1:100:length(max_radius)),'b','LineWidth',2,'MarkerSize',5);
+hold on
+plot([0:100:2.1e05]'*1.0e-05,min_radius(1:100:length(min_radius)),'k','LineWidth',2,'MarkerSize',5);
+hold off
+xlim([0 2.1])
+xlabel('Time')
+ylabel('Distance to the center','interpreter','latex')
+legend('maximum distance','minimum distance','FontSize',14)
 
 function [A,b,t_gmls,n_gmls,rightpt,alpha,center_x,center_y] = velocity(curve,N,told,nold)
 rightpt = zeros(N,1);
@@ -144,6 +179,7 @@ b_int = zeros(N,1);
 b_sint = zeros(N,1);
 x = curve(:,1);
 y = curve(:,2);
+% number of k-nearest points
 if mod(ceil(sqrt(N)),2)==0
     k = ceil(sqrt(N))+1;
 else
@@ -151,11 +187,11 @@ else
 end
 epsilon = 1.0e-12;
 trial=0;
-%approximate tangent vector and normal vector by gmls
+%approximate tangent vector and normal vector by local svd
 [t_rough,n_rough,inds] = localsvd(curve,k,1,told,nold);
 n_rough(1,:) = -t_rough(2,:);
 n_rough(2,:) = t_rough(1,:);
-
+%approximate tangent vector and normal vector by gmls
 [t_gmls6, alpha6] = localgmls(curve,inds,1,t_rough,n_rough,6);
 t_gmls = t_gmls6;
 n_gmls = zeros(size(t_gmls));
@@ -166,7 +202,7 @@ for pp = 1:N
         n_gmls(:,pp) = -n_gmls(:,pp);
     end
 end
-
+% iterate gmls process to make \alpha_1 close to 0
 while max(abs(alpha6(1,:)'))>epsilon
     [t_gmls6, alpha6] = localgmls(curve,inds,1,t_gmls,n_gmls,6);
     t_gmls = t_gmls6;
@@ -188,6 +224,7 @@ alpha(1,:) = 0;
 cur6_gmls =-2*alpha6(2,:)'./((1+alpha6(1,:)'.^2).^(3/2));
 cur_gmls = cur6_gmls;
 
+%integration bound \delta s_i and \delta s_{-i}
 for qq = 1:N-1
   rightpt(qq) = t_gmls(:,qq)'*[x(qq+1)-x(qq);y(qq+1)-y(qq)];
 end
@@ -252,6 +289,7 @@ else
 end
 fovers_lterm = - 1/(4*pi)*log(1+(fovers_leftpt(j))^2)*fprime_leftpt(j)*abs(leftpt(j))/2;
 fovers_rterm = - 1/(4*pi)*log(1+(fovers_rightpt(j))^2)*fprime_rightpt(j)*rightpt(j)/2;
+%singular terms
 int_beta0 = -1/(2*pi)*(-leftpt(j)*log(-leftpt(j))+leftpt(j)+rightpt(j)*log(rightpt(j))-rightpt(j));
 int_beta1 = -1/(2*pi)*(-1/2*leftpt(j)^2*log(-leftpt(j))+1/4*(leftpt(j))^2+1/2*rightpt(j)^2*log(rightpt(j))-1/4*rightpt(j)^2);
 int_beta2 = -1/(2*pi)*(1/3*(-leftpt(j))^3*log(-leftpt(j))-1/9*(-leftpt(j))^3+1/3*rightpt(j)^3*log(rightpt(j))-1/9*rightpt(j)^3);
@@ -310,7 +348,7 @@ end
 b = b_sint./2 + cur_gmls./2;
 end
 
-
+% build multi-indices for multivariate polynomials if necessary
 function index =  generatemultiindex(N,dim)
 % input
 % N : max degree of polynomials
@@ -335,15 +373,14 @@ index1 = find(sum(allindex)<=N);
 index = allindex(:,index1);
 end
 
+% find k-nearest points (can use the built-in function (knn) directly)
 function [ds,inds] = knnCPU(R,Q,k,maxMem)
 % Find the k nearest neighbors for each element of the query set Q among
 % the points in the reference set R
-
 % Q is Nxd where N is the number of query points and d is the dimension
 % R is Mxd where M is the number of reference points and d is the dimension
 % k is the number of nearest neighbors desired
 % maxMem is the number of GB of ram knnCPU can use
-
 % ds is Nxk and contains the distances to the k nearest neighbors.
 % inds is Nxk and contains the indices of the k nearest neighbors.
     if (nargin<4)
@@ -384,6 +421,7 @@ function [ds,inds] = knnCPU(R,Q,k,maxMem)
     ds = real(sqrt(ds));
 end
 
+% GMLS approximation of tangent vectors
 function [t_tilde,alpha] = localgmls(x,inds,d,t_rough,n_rough,ell)
 N = size(x,1);
 n = size(x,2);
@@ -419,6 +457,7 @@ for pp=1:N
         end
      end
 
+     % coefficients in polynomial by least squares
      a1 =(Phi'*Phi)\(Phi'*lcx(:,n));
      alpha(:,pp) = a1;
         
@@ -427,12 +466,13 @@ for pp=1:N
     end
 end
 
+% Get approximate tangent vectors and normal vectors by local SVD method
 function [t_rough,n_rough,inds] = localsvd(x,k0,d,tvec,nvec)
 [~,inds] = knnCPU(x,x,k0);
 N = size(x,1);
 n = size(x,2);
    
-for pp = 1:N   % should be 1:N for all points
+for pp = 1:N
     dx = x(inds(pp,:),:)';
     dx = dx - repmat(x(pp,:)',1,k0); 
     [U,~,~] = svd(dx); 
